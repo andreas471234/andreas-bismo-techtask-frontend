5b68502c4a8ea6978fc97e2e90f94866b07e2b63
andreas45123 <andreas.bismo@pasarpolis.com>
2020-01-24 00:59:07 +0700

    added cors

--- api/app.js
+++ api/app.js
@@ -2,25 +2,35 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var request = require('request');
+var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.get('/ingredients', function(req, res){
+  var url = "https://lb7u7svcm5.execute-api.ap-southeast-1.amazonaws.com/dev/ingredients";
+  request(url, function(err, resp, body) {
+    body = JSON.parse(body);
+    res.send(body)
+  });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +47,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 3000')
+})
 
 module.exports = app;
